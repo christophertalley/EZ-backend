09a768150d2764f79cea28f4f8800b8e18ad51b6
Chris Talley <christopherr.talley@gmail.com>
2020-06-19 13:43:39 -0500

    Slight changes to user route

--- routes/users.js
+++ routes/users.js
@@ -9,19 +9,19 @@ const router = express.Router();
 // database for relationships
 router.post("/users", checkJwt, asyncHandler(async (req, res, next)=>{
     const {username} = req.body;
-    const db = req.db;
+    const client = req.db;
     try {
         // Not quite sure why, but the uri points to the admin database and not my api
         // To get this to work you have to state db again after db and then provide
         // the database name.
 
         // Because the way Mongo works we have to first query to check if the user has been created
-        const findResult = await db.db("ez-api").collection("users").findOne({ userName: username });
+        const findResult = await client.db("ez-api").collection("users").findOne({ userName: username });
         if (findResult) {
             res.status(201).send({ userId: findResult._id });
         } else {
             // If the user was not found, we create a new document with the user
-            const result = await db.db("ez-api").collection("users").insertOne({userName: username});
+            const result = await client.db("ez-api").collection("users").insertOne({userName: username});
             res.status(201).json({ userId: result.insertedId });
         }
     } catch (error) {
