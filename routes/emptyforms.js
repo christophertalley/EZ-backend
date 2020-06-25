117c5a896bd7b30378698b67b30620eb1281d8f8
Chris Talley <christopherr.talley@gmail.com>
2020-06-24 20:02:27 -0500

    small modifications

--- routes/emptyforms.js
+++ routes/emptyforms.js
@@ -1,13 +1,25 @@
 const express = require("express");
 const {checkJwt} = require("../auth");
 const {asyncHandler} = require("../utils");
+const { ObjectID } = require("mongodb");
 
 const router = express.Router();
 
 // This will be the route to get the empty form to be completed
-router.get('/forms/:formId', (req, res, next)=>{
+router.get('/forms/:formId', asyncHandler (async (req, res, next)=>{
+    console.log(req.params);
 
-});
+    const formId = req.params.formId;
+    console.log(formId);
+    const objId = new ObjectID(formId);
+    const client = req.db;
+    try {
+        const form = await client.db("ez-api").collection("emptyForms").findOne({ _id: objId});
+        res.status(200).send({form})
+    } catch (e) {
+        console.error(e);
+    }
+}));
 
 // This route will be used to get all forms for a user
 router.get('/forms', checkJwt, (req, res, next)=> {
